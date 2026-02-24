b3f776c2397efc6fb1204243d0e19831bb19b268
Jerry <j24.yuan@gmail.com>
2026-02-24 15:42:58 +0800

    Initial Commit

---
+++ subscription-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  subscription-tracker
+//
+//  Created by Jerry　 on 2026/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ subscription-tracker/subscription_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  subscription_trackerApp.swift
+//  subscription-tracker
+//
+//  Created by Jerry　 on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct subscription_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
